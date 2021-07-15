98d3064fc20b0f56704829a0e1c16e5621802eea
Nicolás Moreno Posada <moreno.pnicolas@gmail.com>
2021-07-15 17:03:21 -0500

    Changed imports of sections to lazy

--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
-// import { useTranslation } from "react-i18next";
-import AbilitiesSection from "../sections/AbilitiesSection";
-import HomeSection from "../sections/HomeSection";
-import PortfolioSection from "../sections/PortfolioSection";
-import ReferencesSection from "../sections/ReferencesSection";
-import TrajectorySection from "../sections/TrajectorySection";
-// import FloatingButton from "../components/FloatingButton/FloatingButton";
+import React, { useEffect, Suspense, lazy } from "react";
+
 import ROUTES from "../router/routes.json";
 import GoBackTopBtn from "../components/GoBackTopBtn/GoBackTopBtn";
-import Footer from "../sections/Footer";
+import LoadingScreen from "../components/LoadingScreen/LoadingScreen";
+const HomeSection = lazy(() => import("../sections/HomeSection"));
+const PortfolioSection = lazy(() => import("../sections/PortfolioSection"));
+const ReferencesSection = lazy(() => import("../sections/ReferencesSection"));
+const AbilitiesSection = lazy(() => import("../sections/AbilitiesSection"));
+const TrajectorySection = lazy(() => import("../sections/TrajectorySection"));
+
+const Footer = lazy(() => import("../sections/Footer"));
+
 var lastScroll = 0;
 var canHideRef;
 function HomePage({ setScrolling, scrolling, canHide, setCanHide }) {
@@ -56,28 +58,30 @@ function HomePage({ setScrolling, scrolling, canHide, setCanHide }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div
-      data-bs-spy="scroll"
-      data-bs-target="#custom-navbar"
-      data-bs-offset="200"
-      id="#body-container"
-      tabIndex="0"
-      style={{
-        position: "relative",
-        overflowY: "scroll",
-        height: "100vh",
-      }}
-    >
-      {/* <FloatingButton /> */}
-      <GoBackTopBtn setCanHide={setCanHide} scrolling={scrolling} />
-      <div id={ROUTES.HOME} style={{ height: "0" }}></div>
-      <HomeSection setCanHide={setCanHide} />
-      <PortfolioSection />
-      <AbilitiesSection />
-      <TrajectorySection />
-      <ReferencesSection />
-      <Footer />
-    </div>
+    <Suspense fallback={<LoadingScreen />}>
+      <div
+        data-bs-spy="scroll"
+        data-bs-target="#custom-navbar"
+        data-bs-offset="200"
+        id="#body-container"
+        tabIndex="0"
+        style={{
+          position: "relative",
+          overflowY: "scroll",
+          height: "100vh",
+        }}
+      >
+        {/* <FloatingButton /> */}
+        <GoBackTopBtn setCanHide={setCanHide} scrolling={scrolling} />
+        <div id={ROUTES.HOME} style={{ height: "0" }}></div>
+        <HomeSection setCanHide={setCanHide} />
+        <PortfolioSection />
+        <AbilitiesSection />
+        <TrajectorySection />
+        <ReferencesSection />
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
 
