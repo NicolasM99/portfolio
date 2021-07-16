b52954b0d07643c9eff4d12e10c7b791b3582a1d
Nicolás Moreno Posada <moreno.pnicolas@gmail.com>
2021-07-16 10:53:26 -0500

    Improved mobile UI and separated social media icons as own component. TODO: Fix Navbar hiding for mobile

---
+++ src/app/components/SocialMedia/SocialMedia.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import Tooltip from "../Tooltip/Tooltip";
+const social_media = [
+  {
+    name: "Behance",
+    icon: "behance-square",
+    link: "https://www.behance.net/nicolasmoreno99",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    link: "https://www.linkedin.com/in/nicolas-moreno-posada/",
+  },
+  {
+    name: "GitHub",
+    icon: "github-square",
+    link: "https://github.com/NicolasM99",
+  },
+  {
+    name: "Instagram",
+    icon: "instagram-square",
+    link: "https://www.instagram.com/",
+  },
+];
+export default function SocialMedia({ color }) {
+  return (
+    <Row className="social-media-icon-container">
+      {social_media.map((item, idx) => (
+        <Tooltip
+          key={idx}
+          label={item.name}
+          render={(ref, triggerHandler) => {
+            return (
+              <a
+                ref={ref}
+                {...triggerHandler}
+                href={item.link}
+                target="_blank"
+                rel="noreferrer"
+                key={idx}
+              >
+                <i
+                  className={`fab fa-${item.icon} fa-2x social-media-icon`}
+                  style={{ color: color }}
+                />
+              </a>
+            );
+          }}
+        />
+      ))}
+    </Row>
+  );
+}
--- src/app/sections/HomeSection.js
+++ src/app/sections/HomeSection.js
@@ -5,36 +5,7 @@ import { Link } from "react-router-dom";
 import { scrollWithOffset } from "../util/scrollWithOffset";
 import { useTranslation } from "react-i18next";
 import cube from "../../assets/images/cube.png";
-import Tooltip from "../components/Tooltip/Tooltip";
-
-const social_media = [
-  {
-    name: "Behance",
-    icon: "behance-square",
-    link: "https://www.behance.net/nicolasmoreno99",
-  },
-  {
-    name: "LinkedIn",
-    icon: "linkedin",
-    link: "https://www.linkedin.com/in/nicolas-moreno-posada/",
-  },
-  {
-    name: "GitHub",
-    icon: "github-square",
-    link: "https://github.com/NicolasM99",
-  },
-  {
-    name: "Instagram",
-    icon: "instagram-square",
-    link: "https://www.instagram.com/",
-  },
-];
-
-export const renderTooltip = (props, label) => (
-  <Tooltip id="button-tooltip" {...props}>
-    {label}
-  </Tooltip>
-);
+import SocialMedia from "../components/SocialMedia/SocialMedia";
 
 function HomeSection({ setCanHide }) {
   const [show, setShow] = useState(false);
@@ -69,30 +40,7 @@ function HomeSection({ setCanHide }) {
         </Col>
         <Col lg={4} className="text-center">
           <Image id="cube-img" src={cube} alt="cube-img" />
-          <Row className="social-media-icon-container">
-            {social_media.map((item, idx) => (
-              <Tooltip
-                key={idx}
-                label={item.name}
-                render={(ref, triggerHandler) => {
-                  return (
-                    <a
-                      ref={ref}
-                      {...triggerHandler}
-                      href={item.link}
-                      target="_blank"
-                      rel="noreferrer"
-                      key={idx}
-                    >
-                      <i
-                        className={`fab fa-${item.icon} fa-2x social-media-icon`}
-                      />
-                    </a>
-                  );
-                }}
-              />
-            ))}
-          </Row>
+          <SocialMedia />
           <Row className="home-buttons-container">
             <div>
               <Button variant="outline-primary">
