1af2db6d9c5d340175f531e81e2fd6f9795cb1ec
Nicolás Moreno Posada <moreno.pnicolas@gmail.com>
2021-07-24 18:37:27 -0500

    Starting portfolio section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./app/router/AppRouter";
 import "./app/i18next/i18n";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function App() {
   return (
